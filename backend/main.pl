5d5a18a9bf3cf161f04f7d8e72a4fea8bcd1d09a
Yhas-04 <yhasmin.com.br@gmail.com>
2026-05-06 17:40:36 -0300

    commit

--- backend/main.pl
+++ backend/main.pl
@@ -30,3 +30,60 @@ pontuacao_base(trinca, 100).
 pontuacao_base(dois_pares, 60).
 pontuacao_base(par, 30).
 pontuacao_base(carta_alta, 10).
+
+meta(1, 100).
+meta(2, 250).
+meta(3, 500).
+
+max_descartes(3).
+
+
+estado(1, 0, 0).
+
+avaliar_mao(Cartas, Mao, Pontos) :-
+    pegar_faces(Cartas, Faces),
+    pegar_naipes(Cartas, Naipes),
+    pegar_valores(Faces, Valores),
+    escolher_mao(Faces, Naipes, Valores, Mao),
+    pontuacao_base(Mao, Base),
+    length(Cartas, Quantidade),
+    Bonus is (Quantidade - 1) * 5,
+    Pontos is Base + Bonus.
+
+
+escolher_mao(Faces, Naipes, _, royal_flush) :-
+    tem_flush(Naipes),
+    member(10, Faces),
+    member(valete, Faces),
+    member(dama, Faces),
+    member(rei, Faces),
+    member(as, Faces), !.
+
+      member(as, Faces), !.
+
+escolher_mao(_, Naipes, Valores, straight_flush) :-
+    tem_flush(Naipes),
+    tem_sequencia(Valores), !.
+
+escolher_mao(Faces, _, _, quadra) :-
+    tem_repetidas(Faces, 4), !.
+
+escolher_mao(Faces, _, _, full_house) :-
+    tem_full_house(Faces), !.
+
+escolher_mao(_, Naipes, _, flush) :-
+    tem_flush(Naipes), !.
+
+escolher_mao(_, _, Valores, sequencia) :-
+    tem_sequencia(Valores), !.
+
+escolher_mao(Faces, _, _, trinca) :-
+    tem_repetidas(Faces, 3), !.
+
+escolher_mao(Faces, _, _, dois_pares) :-
+    tem_dois_pares(Faces), !.
+
+escolher_mao(Faces, _, _, par) :-
+    tem_repetidas(Faces, 2), !.
+
+escolher_mao(_, _, _, carta_alta).
